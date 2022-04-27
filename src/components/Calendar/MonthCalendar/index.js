cd1084073bdd0b6c93c0e3203f2a0bd84aa3d859
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-04-27 12:02:42 +0300

    Add base structure

--- src/components/Calendar/MonthCalendar/index.js
+++ src/components/Calendar/MonthCalendar/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import style from './MonthCalendar.module.scss'
 
 const MonthCalendar = (props) => {
-  return <div className={style['calendar-container']}>{props.month}</div>
+  return <div className={style['calendar-container']}>
+    {props.year}
+    {props.month}
+    {props.week}
+    </div>
 }
 
 export default MonthCalendar
